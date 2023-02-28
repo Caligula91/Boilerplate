02f0cfe6f3d97afdf1e87e38035a1d403d72eb37
Mihajlo Nikolic <mihajlo.nikolic.991@gmail.com>
2023-03-01 00:36:05 +0100

    feat(test): better db cleanup before and after test

--- tests/clearDatabase.js
+++ tests/clearDatabase.js
@@ -1,17 +1,40 @@
 const mongoose = require('mongoose');
+const { before, after } = require('mocha');
+const mongoDB = require('../config/database/mongodb/connection');
 
 before((done) => {
-  mongoose.connection.on('open', () => {
-    mongoose.connection.db.dropDatabase(() => {
-      done();
-    });
+  mongoose.connect(mongoDB.connectionString());
+
+  mongoose.connection.once('open', () => {
+    mongoose.connection.db.dropDatabase()
+      .catch((err) => console.log(err))
+      .finally(() => done());
+  });
+
+  process.on('SIGINT', () => {
+    try {
+      mongoose.connection.db.dropDatabase()
+        .catch((err) => console.log(err))
+        .finally(() => {
+          mongoose.connection.close()
+            .then(() => console.log('Mongoose default connection disconnected through app termination'))
+            .catch((err) => console.log(err))
+            .finally(() => process.exit(0));
+        });
+    } catch (err) {
+      console.log(err);
+      process.exit(0);
+    }
   });
 });
 
-after((done) => {
-  mongoose.connection.db.dropDatabase(() => {
+after(async () => {
+  try {
+    await mongoose.connection.db.dropDatabase();
     console.log('MongoDB collections dropped');
-    mongoose.disconnect();
-    done();
-  });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoose.disconnect();
+  }
 });
